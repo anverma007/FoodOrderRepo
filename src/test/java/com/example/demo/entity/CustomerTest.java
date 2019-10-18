33b3f950d8ba7fc0ccdb0b6be006f61d266d4991
Alok Verma <M1049007@mindtree.com>
2019-10-18 15:19:15 +0530

    Updated Junit testcases

--- src/test/java/com/example/demo/entity/CustomerTest.java
+++ src/test/java/com/example/demo/entity/CustomerTest.java
@@ -1,5 +1,7 @@
 package com.example.demo.entity;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,16 +25,19 @@ public class CustomerTest {
 	@Test
 	public void getCust_IdTest() {
 		int id = customer.getCust_Id();
+		assertNotNull(id);
 	}
 	
 	@Test
 	public void getCust_NameTest() {
 		String name = customer.getCust_Name();	
+		assertNotNull(name);
 	}
 	
 	@Test
 	public void getLocationTest() {
 		String loc = customer.getLocation();
+		assertNotNull(loc);
 	}
 	
 }
